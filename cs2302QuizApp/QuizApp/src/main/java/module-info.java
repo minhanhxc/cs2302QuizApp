20eaff83f0014e0275ef1b3fe701caa1d743f08f
minhanhxc <144601707+minhanhxc@users.noreply.github.com>
2025-06-19 00:56:05 +0700

    Delete cs2302QuizApp directory

--- cs2302QuizApp/QuizApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.minhanh.quizapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.minhanh.quizapp to javafx.fxml;
-    exports com.minhanh.quizapp;
-}
